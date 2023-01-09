47583106a40c5db62a69ee72885e67958e11d864
lim.cs <cheolsoon.lim@emotion.co.kr>
2023-01-09 12:31:59 +0900

    error_handling

---
+++ src/Error_ handling/3_shot_cut_handling.rs
@@ -0,0 +1,39 @@
+use std::fs::File;
+use std::io;
+use std::io::Read;
+fn main() {
+    //1
+    fn read_username_from_file() -> Result<String, io::Error> {
+        let f = File::open("hello.txt");
+        let mut f = match f {
+            Ok(file) => file,
+            Err(e) => return Err(e),
+        };
+        let mut s = String::new();
+        match f.read_to_string(&mut s) {
+            Ok(_) => Ok(s),
+            Err(e) => Err(e),
+        }
+    }
+    let read_file = read_username_from_file();
+    println!("{:?}", read_file);
+
+    //2 error_handling_shot_cut
+    fn read_username_from_file2() -> Result<String, io::Error> {
+        let mut f = File::open("hello.txt")?;
+        let mut s = String::new();
+        f.read_to_string(&mut s)?;
+        Ok(s)
+    }
+    let read_file = read_username_from_file2();
+    println!("{:?}", read_file);
+
+    //3
+    fn read_username_from_file3() -> Result<String, io::Error> {
+        let mut s = String::new();
+        File::open("hello.txt")?.read_to_string(&mut s)?;
+        Ok(s)
+    }
+    let read_file = read_username_from_file3();
+    println!("{:?}", read_file);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,37 @@
-enum Temperature {
-    Celsius(i32),
-    Fahrenheit(i32),
-}
-
 fn main() {
-    let temperature = Temperature::Celsius(35);
-    // ^ TODO try different values for `temperature`
-
-    match temperature {
-        Temperature::Celsius(t) if t > 30 => println!("{}C is above 30 Celsius", t),
-        // The `if condition` part ^ is a guard
-        Temperature::Celsius(t) => println!("{}C is below 30 Celsius", t),
-
-        Temperature::Fahrenheit(t) if t > 86 => println!("{}F is above 86 Fahrenheit", t),
-        Temperature::Fahrenheit(t) => println!("{}F is below 86 Fahrenheit", t),
+    //1
+    let numbers = vec![34, 50, 25, 100, 65];
+    let mut largest = numbers[0];
+    for number in numbers {
+        if number > largest {
+            largest = number;
+        }
     }
+    println!("The largest number is {}", largest);
+    //2
+    let numbers = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+    let mut largest = numbers[0];
+    for number in numbers {
+        if number > largest {
+            largest = number;
+        }
+    }
+    println!("The largest number is {}", largest);
+    //3    1,2가 중복
+    fn find_largest(list: &[i32]) -> i32 {
+        let mut largest = list[0];
+        for &item in list.iter() {
+            if item > largest {
+                largest = item;
+            }
+        }
+        largest
+    }
+
+    let numbers = vec![34, 50, 25, 100, 65];
+    let result = find_largest(&numbers);
+    println!("The largest number is {}", result);
+    let numbers = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+    let result = find_largest(&numbers);
+    println!("The largest number is {}", result);
 }
